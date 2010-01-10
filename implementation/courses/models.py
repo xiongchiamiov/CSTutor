b6797899e78bdf8bd8ec0b032a7a06ee3c3d0133
mgius <mgius@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-10 23:58:48 +0000

    Added stub models to courses
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@616 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/courses/models.py
+++ implementation/courses/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Course():
-   pass
+class Course(models.Model):
+	pass
 
+class Lesson(models.Model):
+	pass
+
+class Quiz(models.Model):
+	pass
