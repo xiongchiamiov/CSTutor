1c01caded6d16db21e199075bf7b8475e0564aef
ekleist <ekleist@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-11 00:04:55 +0000

    Stubbed
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@618 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/users/models.py
+++ implementation/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Models):
+	''' A user contains a boolean determining whether or not the user has instructor permissions, a string for the username, a string for the password, and a list of Courses that the user is enrolled in. Additionally, a user may provide an email address.
+	'''
+	pass
