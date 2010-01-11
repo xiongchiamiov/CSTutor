bca406630d4d00d117044b121d25924a0fd90d42
mgius <mgius@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-11 00:15:49 +0000

    Adding ignores for pyc files
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@620 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/courses/models.py
+++ implementation/courses/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Course(models.Model):
+	''' 
+		A course is a set of zero or more pages.  A course is either public or
+	   private, and contains a roster of authorized users.  Every course has a
+	   landing page. 
+	'''
 	pass
 
 class Lesson(models.Model):
