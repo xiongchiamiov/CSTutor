371d4993814039e3adfedc603f15751eee5df41a
mgius <mgius@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-13 23:13:11 +0000

    Fixed docstring on models
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@631 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/users/models.py
+++ implementation/users/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 
 class User(models.Models):
-	''' A user contains a boolean determining whether or not the user has instructor permissions, a string for the username, a string for the password, and a list of Courses that the user is enrolled in. Additionally, a user may provide an email address.
+	''' 
+		Model for a User
+
+		A user contains a boolean determining whether or not the user has 
+		instructor permissions, a string for the username, a string for the 
+		password, and a list of Courses that the user is enrolled in.
+		Additionally, a user may provide an email address.
 	'''
 	pass
