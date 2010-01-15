412afab624f048df477340eeec7971105eb5cf6f
jinloes <jinloes@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-15 23:35:19 +0000

    milestone 2 stubs
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@644 50065d94-6802-42e0-adc3-1bef15d4fe63

---
+++ implementation/courses/roster.py
@@ -0,0 +1,23 @@
+def addUser(newUser):
+	''' 
+	Adds the given string name into the Roster and produces an updated Roster.
+	'''
+pass
+
+def removeUser(user):
+	''' 
+	Removes the inputed string names from the roster and produces an updated Roster.
+	'''
+pass
+
+def setUserPermissions(user, permissions):
+	''' 
+	Takes in a list of tuples consisting of a string name, boolean edit, boolean manage, and boolean states and will produce an updated Roster with the new user permissions.
+	'''
+pass
+
+def updateRoster(roster):
+	''' 
+	Takes in a roster and update the user database.
+	'''
+pass
