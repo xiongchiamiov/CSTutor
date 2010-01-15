72efba9398246c1fed7a0ae03cc8bdab73a20d91
jhartqui <jhartqui@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-15 20:49:13 +0000

    stubbed user.py
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@636 50065d94-6802-42e0-adc3-1bef15d4fe63

---
+++ implementation/users/user.py
@@ -0,0 +1,40 @@
+
+
+def editProfile(oldUser, newUser):
+   '''
+   This operation takes a users profile and any modifications and 
+   merges them together, returning an updated profile
+   '''
+   pass
+
+def login(userDB, username, password):
+   '''
+   This operation checks a username and password against the user database
+   and decides if the user can login.
+   '''
+   pass
+
+def logout():
+   '''
+   Logs a user out.
+   '''
+   pass
+
+def createUser(username, userDB):
+   '''
+   Creates a new user and adds it to the user database
+   '''
+   pass
+
+def removeUser(username, userDB):
+   '''
+   Removes the given user from the user database
+   '''
+   pass
+
+
+def setInstructor(user, instructor):
+   '''
+   Grants a user permission to create a course
+   '''
+   pass
