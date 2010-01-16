9d05dee2201e899e98cfe85cd5387949a69f084a
mtytel <mtytel@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-16 00:24:21 +0000

    added course.py with stubs
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@650 50065d94-6802-42e0-adc3-1bef15d4fe63

---
+++ implementation/courses/course.py
@@ -0,0 +1,74 @@
+'''
+course.py file for page related operations.
+
+Contains operations for Courses
+
+Author(s): Matthew Tytel
+
+'''
+
+def removePage(request):
+	'''
+	Removes the specified page from the specified course
+
+	This operation returns an http response for viewing the deletion
+	'''
+	pass
+
+def getPrevPage(request):
+	'''
+	Creates a new course with the specified parameters
+
+	This operation returns an http response for viewing the new course
+	'''
+	pass
+
+def removeCourse(request):
+	'''
+	Removes a specified course from existence
+
+	This operation returns an http response for viewing the deletion of the course
+	'''
+	pass
+
+def addPage(request):
+	'''
+	Adds a page to an existing specified Course
+
+	This operation returns an http response for viewing the addition
+	'''
+	pass
+
+def setPrivate(request):
+	'''
+	Set's the class to public/private depending on what is specified
+
+	This operation returns an http response for viewing the privacy change
+	'''
+	pass
+
+def search(request):
+	'''
+	Searches a specified class for a page
+
+	This operation returns an http response for viewing the search results
+	'''
+	pass
+
+def import(request):
+	'''
+	Imports specified information into a Course
+
+	This operation returns an http response for viewing the import
+	'''
+	pass
+
+def export(request):
+	'''
+	Exports the specified Course
+
+	This operation returns an http response with the export data
+	'''
+	pass
+
+
