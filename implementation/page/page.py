b0b0a6d65239ffa8a4671ec08f6dfd5740596c52
mtytel <mtytel@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-16 00:16:53 +0000

    page.py stubbing
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@649 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/page/page.py
+++ implementation/page/page.py
@@ -1,1 +1,49 @@
-# Create your views here.
+'''
+page.py file for page related operations.
+
+Contains operations for all pages
+
+Author(s): Matthew Tytel
+
+'''
+
+def getNextPage(request):
+	'''
+	Gets the next page in the preorder traversal of the page hierarchy
+
+	This operation returns an http response for viewing the next page
+	'''
+	pass
+
+def getPrevPage(request):
+	'''
+	Gets the previous page in the preorder traversal of the page heirarchy
+
+	This operation returns an http response for viewing the previous page
+	'''
+	pass
+
+def displayPage(request):
+	'''
+	Gets a page in the request
+
+	This operation returns an http response for viewing the requested page
+	'''
+	pass
+
+def movePage(request):
+	'''
+	Moves the specified to a new location specified by the new location's previous page
+
+	This operation returns an http response for viewing the move
+	'''
+	pass
+
+def createPage(request):
+	'''
+	Creates a new page with the specified parameters
+
+	This operation returns an http response for viewing the new page
+	'''
+	pass
+
