f49d425c5a49ed03bf940bc38d499d57dcd3ec14
mgius <mgius@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-18 00:10:02 +0000

    Modifying settings.py
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@661 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/settings.py
+++ implementation/settings.py
@@ -45,7 +45,7 @@ MEDIA_URL = ''
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/adminmedia/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '_2orc=nbr%73)uzltzgvbz=7p1&-^(btvxe(mj%l1n8st@os7z'
