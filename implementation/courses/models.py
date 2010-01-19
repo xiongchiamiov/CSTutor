d147aab4acc2622556c057c33f9a3b7341ced89d
mgius <mgius@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-19 01:28:20 +0000

    james and I worked this out on Sunday.  It may not be 100% correct
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@664 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/courses/models.py
+++ implementation/courses/models.py
@@ -8,6 +8,7 @@ SVN Commit Info:
 $Id$
 '''
 from django.db import models
+from users.model import User
 
 # Create your models here.
 
@@ -19,21 +20,33 @@ class Course(models.Model):
 	private, and contains a roster of authorized users.  Every course has a
 	landing page. 
 	'''
-	pass
+	slug = models.SlugField(unique = True)
+	name = models.CharField(maxlength = 255)
 
 class Roster(models.Model):
 	'''
 	A roster keeps track of the permissions and statistics for all associated users. A user is linked to stats and permissions.
 	'''
-	roster = {}
+	pass
+
+class Enrollment(models.Model):
+	user = models.ManyToMany(User)
+	permission = models.ManyToMany(Permissions)
+	course = models.ForeignKey(Roster)
 
 class Permissions:
 	'''
-	Model for a Lesson.
+	Model for Permissions for an Enrollment
 
-	Permissions are a set of booleans that map the permissions for users. The view permission allows the user to view the course material. The edit permission allows the user to edit the course material. The stats permission allows the user to view class-wide statistics and roster. The manage permission allows the user to modify the roster and all associated permissions.
+	Permissions are a set of booleans that map the permissions for users. 
+	The view permission allows the user to view the course material. 
+	The edit permission allows the user to edit the course material. 
+	The stats permission allows the user to view class-wide statistics and 
+	roster. 
+	The manage permission allows the user to modify the roster and all 
+	associated permissions.
 	'''
-	view = False
-	edit = False
-	stats = False
-	manage = False
+	view = models.BooleanField
+	edit = models.BooleanField
+	stats = models.BooleanField
+	manage = models.BooleanField
