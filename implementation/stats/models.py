d30f5f6c93e252eae94f41a3c5d939480df1d701
amusselm <amusselm@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-20 21:34:56 +0000

    Added some docs to the stats file
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@679 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/stats/models.py
+++ implementation/stats/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class QuizResponse (models.Model):
-	pass
+	''' Model for a student's response to an individual quiz question.
 
+		There should be one of these for every question in a given quiz. It is
+		used to calculate the user statistics upstream. 
+	'''
+	pass
 
