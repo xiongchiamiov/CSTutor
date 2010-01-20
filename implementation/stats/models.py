0a4ab6d9eb993d6c211e8f1e5b59ffa5341dee41
amusselm <amusselm@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-20 21:45:53 +0000

    Added some fields to the StatAnswer class
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@680 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/stats/models.py
+++ implementation/stats/models.py
@@ -1,11 +1,23 @@
+'''
+Models file for the stats and related classes. 
+
+These store and load user statistics. 
+'''
 from django.db import models
+from users.models import User
+from page.models import Quiz
+from page.models import Question
 
 # Create your models here.
-class QuizResponse (models.Model):
+class StatAnswer (models.Model):
 	''' Model for a student's response to an individual quiz question.
 
 		There should be one of these for every question in a given quiz. It is
 		used to calculate the user statistics upstream. 
 	'''
-	pass
+	student = models.ForeignKey(User)
+	quiz = models.ForeignKey(Quiz)
+	question = models.ForeignKey(Question)
+	
 
+	
