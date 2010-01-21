4744c6470f5084411b86c6ca32b8b106635682fd
mgius <mgius@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-21 00:24:09 +0000

    Added CreateStat method
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@686 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/stats/models.py
+++ implementation/stats/models.py
@@ -11,8 +11,21 @@ from page.models import Page
 # Create your models here.
 
 class stat(models.Model):
-	user = models.ForeignKey(User, related_name='stats')
 	course = models.ForeignKey(Course, related_name='stats')
-	lesson = models.ForeignKey(Page, related_name='stats')
+	page = models.ForeignKey(Page, related_name='stats')
+	user = models.ForeignKey(User, related_name='stats')
 	score = models.IntegerField()
 	date = models.DateTimeField(auto_now_add=True)
+
+	@staticmethod
+	def CreateStat(course_, page_, user_, score):
+		''' Create a new Stat Entry
+
+			 Creates a new entry in the stats table for the passed course, page,
+			 and user.  The entry is timestamped automatically.
+
+			 Returns the statistic after saving it to the database
+		'''
+		s = stat(course=course_, page=page_, user=user_, score=score_)
+		s.save()
+		return s
