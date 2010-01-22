19ff322b780945eae52dfb0f972d8f794e44ceb1
ekleist <ekleist@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-22 21:49:49 +0000

    Added quiz stuff
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@706 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/page/views.py
+++ implementation/page/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from page.models import page
+
+def show_page(request, course, courses, pid):
+	page = page.objects.get(slug=pid)
+	
+	if page.__class__ == 'page.models.Quiz':
+		return show_quiz(request, course, courses, pid)
+
+def show_quiz(request, course, courses, pid):
+	return render_to_response('quiz/index.html', {'course':course, 'courses':courses, 'pid':pid})
+
