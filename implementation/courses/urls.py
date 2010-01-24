a38723ed904794e81b24167b327d06772d1449b9
rmezzett <rmezzett@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-24 23:06:37 +0000

    I made the coursename/pagename/ url work
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@717 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/courses/urls.py
+++ implementation/courses/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
+#if the original url was "/coursename/pagename/"
+#the expression here would be "pagename/"
+#if it were just "/coursename/" then it would be "" (an empty string)
+
 urlpatterns = patterns('courses.views',
 	(r'roster/$', 'show_roster'),
-	(r'^(?P<pid>[\w-]+)/', include('page.urls')),
+	(r'(?P<pid>[\w-]+)/', include('page.urls')),
 	(r'', 'show_course'),
 )
+
