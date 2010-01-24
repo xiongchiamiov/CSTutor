4b6ae883d3f714a1e6a0333823cc8fe642b92898
jinloes <jinloes@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-24 22:55:06 +0000

    fixed redirection
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@716 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/courses/urls.py
+++ implementation/courses/urls.py
@@ -3,6 +3,6 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('courses.views',
 	(r'roster/$', 'show_roster'),
-	(r'^(?P<pid>[\w-]+)/', include('page.urls'), courses????#fix
-	(r'/$', 'show_course'),
+	(r'^(?P<pid>[\w-]+)/', include('page.urls')),
+	(r'', 'show_course'),
 )
