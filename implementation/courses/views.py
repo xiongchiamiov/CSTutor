11652fa93b870b4ae88cd093f83a49d53b1bdd57
jinloes <jinloes@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-25 01:36:01 +0000

    added adduser stuff
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@740 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/courses/views.py
+++ implementation/courses/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
 from courses.models import Course
 
 def show_roster(request, course_slug, courses):
 	course = Course.objects.get(slug=course_slug)
 	enrollments = course.roster.all();
-	#userList = []
-
-
-	#for enrollment in enrollments:
-		#userList.append(enrollment.user.first_name + ' ' + enrollment.user.last_name)
 
 	return render_to_response('roster/index.html', {'course': course, 'enrollments': enrollments, 'courses': courses, 'course_slug': course.slug})
 
 def show_course(request, courses, course_slug):
 	return render_to_response('index.html', {'courses': courses, 'course_slug': course_slug})
+
+def add_user(request, course_slug, courses):
+	if request.method == 'POST':
+		firstname = request.POST['firstname']
+		lastname = request.POST['lastname']
+		
+		user = User()
+		user.first_name = firstname
+		user.last_name = lastname
+		
+		try:
+			user.save()
+		except:
+			pass
+
+		course = Courses.object.get(course_slug)
+		addUser(course, user)
+		return HttpResponseRedirect("/%s/roster/" % course_slug)
+	else:
+		course = Course.objects.get(slug=course_slug)
+		return render_to_response('adduser/index.html', {'course_slug': course_slug, 'courses':courses})
