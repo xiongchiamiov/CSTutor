208228232de6fc02270ad94059b7727b3356968f
jhartqui <jhartqui@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-25 11:42:25 +0000

    m3
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@768 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/users/views.py
+++ implementation/users/views.py
@@ -1,4 +1,19 @@
 # Create your views here.
 
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
+
 def show_profile(request):
-	pass
+      print 'in show_profile'
+
+      if request.method == 'POST':
+            usr = request.POST['username']
+
+            try:
+                  user = User.objects.get(username=usr)
+                  return render_to_response('user/profile.html', {'user':user})
+            except User.DoesNotExist:
+                  return render_to_response('user/notfound.html')
+      else:
+            return render_to_response('user/lookup.html')
