1b7129809792c311f96153c154fea7cf98d9f0a5
ekleist <ekleist@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-01-25 00:16:59 +0000

    page views
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@732 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/page/quiz/views.py
+++ implementation/page/quiz/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+
+def show_quiz(request, course, courses, pid):
+	return render_to_response('quiz/index.html', {'course':course, 'courses':courses, 'pid':pid})
+
--- implementation/page/views.py
+++ implementation/page/views.py
@@ -2,23 +2,19 @@ from django.shortcuts import render_to_response
 #from page.models import Page
 from models import Page
 #from page.lesson.views import show_lesson
-from lesson.views import show_lesson
+#from lesson.views import show_lesson
+from quiz.views import show_quiz
+from courses.views import show_course
 
 def show_page(request, course, courses, pid):
-
-	#take these two lines out when we have data to work with
-	if 1 == 1:	
-		return show_lesson(request, course, courses, pid)
-	
 	
 	page = Page.objects.get(slug=pid)
 
-	if page.__class__ == 'page.models.Quiz':
+	if page.__class__ == 'page.quiz.models.Quiz':
 	#should be page.quiz.models.Quiz b/c of new folder structure?
 		return show_quiz(request, course, courses, pid)
 	elif page.__class__ == 'page.lesson.models.Lesson':
 		return show_lesson(request, course, courses, pid)
-
-def show_quiz(request, course, courses, pid):
-	return render_to_response('quiz/index.html', {'course':course, 'courses':courses, 'pid':pid})
+	else:
+		return show_course(request, course, courses)
 
