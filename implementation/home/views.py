bb1e28afa3c7147e8c986e82717496f46ad5a0ee
mtytel <mtytel@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-02-01 20:57:35 +0000

    Master Render to Response
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@879 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/home/views.py
+++ implementation/home/views.py
@@ -1,8 +1,15 @@
+''' @author James Pearson, Matt Tytel '''
+
 from django.shortcuts import render_to_response
 from courses.models import Course
 
+def master_rtr(request, template, data = {}):
+	data['courses'] = [e.course for e in request.user.enrollments.all()]
+	print data['courses']
+	import pdb; pdb.set_trace()
+
+	return render_to_response(template, data)
+
 def index(request):
-	data = {}
-	data['courses'] = Course.objects.all()
 	request.session.set_test_cookie()
-	return render_to_response('index.html', data)
+	return master_rtr(request, 'index.html')
