fc606c230387db27704d04aa1696f9ae0d35528f
mgius <mgius@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-02-01 01:01:58 +0000

    Added setup to tests.py
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@853 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/pages/tests.py
+++ implementation/pages/tests.py
@@ -7,27 +7,31 @@ from django.test import TestCase
 class PageTestCase(TestCase):
 	''' Unit tests and other tests on the Page class and it's related functions
 	'''
+	def setUp(self):
+		''' Sets up environment for tests '''
+		pass
+
    def addPageAsSiblingTest(self):
     	"""
 		Tests adding a new page to the tree as a sibling of an existing
 		page
       """
-	pass
+		pass
 
    def addPageAsChildTest(self):
     	"""
 		Tests adding a new page to the tree as a child of an existing page
       """
-	pass
+		pass
 
    def removePageTest(self):
     	"""
 		Tests removing a page from the tree
       """
-	pass
+		pass
 
    def removeTwoPagesTest(self):
     	"""
 		Tests removing several pages from the tree
       """
-	pass
+		pass
