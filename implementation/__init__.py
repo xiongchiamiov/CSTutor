fdb48fcb2480f3ef15c52ed7321de440b954a531
jinloes <jinloes@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-02-01 03:38:18 +0000

    updated the init for testing overview
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@858 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/__init__.py
+++ implementation/__init__.py
@@ -5,4 +5,9 @@
 	<b>views.py<b>: functions called by the user by hitting various site URLs
 	<b>classname.py<b>: methods for each class defined in models.py
 
+	CSTutor uses the DJango testing suit for unit testing. This provides framework to test
+	function-unit testing, class-level inter-function testing, 
+	system-level integration testing, and supports regression testing. 
+	In addition, it can simluate front-end user interaction as well as back-end function testing.
+
 	'''
