255d84065a8d7d1e1eeb522dd2e9ee6dd317f2b9
ekleist <ekleist@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-02-01 20:36:45 +0000

    Fixed the broken page view
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@875 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/pages/lesson/views.py
+++ implementation/pages/lesson/views.py
@@ -5,7 +5,8 @@ def create_lesson(request):
 	print "Create Lesson\n" #TODO
 	return render_to_response('lesson/create-lesson.html', {'courses': Course.objects.all()})
 
-def show_lesson(request, course, courses, pid, lessonContent):
+def show_lesson(request, course, courses, pid):
+  # To get lessonConent now, you need to retreieve the page from the database, cast it to a lesson, and get the "text" attribute
 	return render_to_response('page/lesson/index.html', 
-									{'course':course, 'courses':courses, 'pid':pid, 'lessonContent': lessonContent})
+									{'course':course, 'courses':courses, 'pid':pid})
 
