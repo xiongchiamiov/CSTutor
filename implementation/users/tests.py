f598e2a276a2f5ad440fcf4c837844617f51c83a
jhartqui <jhartqui@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-02-01 09:58:58 +0000

    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@866 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/users/tests.py
+++ implementation/users/tests.py
@@ -9,6 +9,9 @@ from django.test.client import Client
 from django.contrib.auth.models import User
 
 class UserTests(unittest.TestCase):
+	username = 'jhartquist'
+	password = 'password'
+	
 	def setUp(self):
 		'''
 		Sets up the tests
@@ -21,7 +24,43 @@ class UserTests(unittest.TestCase):
 		Tests that a user can login successfully
 		'''
 		username = 'jhartquist'
-                password = 'password'
-                response = self.client.post("/login/", {'username'=username,
-                                                        'password'=password})
-                self.failUnlessEqual(response.status_code, 200)
+		password = 'password'
+		response = self.client.post("/login/", {'username': username,
+                                                'password': password})
+		self.failUnlessEqual(response.status_code, 200)
+
+	def testLogout(self):
+		'''
+		Tests that a user can logout successfully
+		'''
+		username = 'jhartquist'
+		password = 'password'
+		response = self.client.post("/logout/") 
+		self.failUnlessEqual(response.status_code, 200)
+
+	def testShowProfile(self):
+		'''
+		Tests that a user can view their profile
+		'''
+		username = 'jhartquist'
+		password = 'password'
+		self.client.post("/login/", {'username': username,
+                                     'password': password})
+		response = self.client.get("/profile/")
+		self.failUnlessEqual(response.status_code, 200)
+		
+	def testUpdateEmail(self):
+		'''
+		Tests that a user can change their e-mail address
+		'''
+		badEmail = "anemail@nothing"
+		goodEmail = "jhartqui@calpoly.edu"
+		username = 'jhartquist'
+		password = 'password'
+		
+		self.client.post("/login/", {'username': username,
+                                     'password': password})
+		
+		response = self.client.post("/profile/", {'form': "Change E-mail", 'email': badEmail })
+		print(response.content)
+		self.failIfEqual(response.content.find("Invalid E-mail Address"), -1)
