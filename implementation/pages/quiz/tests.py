278b3dd9ad0b917895a76ac269b4a23cd631c7d4
ekleist <ekleist@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-02-07 22:02:59 +0000

    Added some more tests
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@975 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/pages/quiz/tests.py
+++ implementation/pages/quiz/tests.py
@@ -13,8 +13,11 @@ class QuizViewTests(unittest.TestCase):
 		Unit Tests on Quiz Views.  Tests use an emulated Web Client
 		to simulate a user making requests via the web interface
 
-		@author EVan Kleist
-	'''	
+		@author Evan Kleist
+	'''
+	courseSlug = 'gene-fishers-cpe102-fall-08'
+	quizSlug = 'samplequiz'
+
 	def setUp(self):
 		'''
 			Set up the tests
@@ -26,9 +29,36 @@ class QuizViewTests(unittest.TestCase):
 	def testQuizUrl(self):
 		'''
 			Test that the urls to a known quiz page works properly
+			Case no.    Input                                            Expected Output         Remark
+			1           url = /gene-fishers-cpe102-fall-08/samplequiz/   200                     302 is a found code
+			2           url = /badclass/badquiz/                         404                     404 is a bad link error
+			3           url = /gene-fishers-cpe102-fall-08/badquiz/      404                     404 is a bad link error
+			4           url = /badclass/samplequiz/                      404                     404 is a bad link error
 		'''
-		courseSlug = 'gene-fishers-cpe102-fall-08'
-		quizSlug = 'samplequiz'
-		response = self.client.get('/' + courseSlug + '/' + quizSlug + '/')
+		response = self.client.get('/' + self.courseSlug + '/' + self.quizSlug + '/')
 		self.failUnlessEqual(response.status_code, 200)
 
+	def testSubmitQuizUrl(self):
+		'''
+			Test that the urls to submit a known quiz page works properly
+			Case no.    Input                                                       Expected Output         Remark
+			1           url = /gene-fishers-cpe102-fall-08/samplequiz/submitQuiz/   200                     302 is a found code
+			2           url = /badclass/badquiz/submitQuiz/                         404                     404 is a bad link error
+			3           url = /gene-fishers-cpe102-fall-08/badquiz/submitQuiz/      404                     404 is a bad link error
+			4           url = /badclass/samplequiz/submitQuiz/                      404                     404 is a bad link error
+		'''
+		response = self.client.get('/' + self.courseSlug + '/' + self.quizSlug + '/submitQuiz/')
+		self.failUnlessEqual(response.status_code, 200)
+
+	def testEditQuizUrl(self):
+		'''
+			Test that the urls to edit a known quiz page works properly
+			Case no.    Input                                                Expected Output         Remark
+			1           url = /gene-fishers-cpe102-fall-08/samplequiz/edit   200                     302 is a found code
+			2           url = /badclass/badquiz/edit/                        404                     404 is a bad link error
+			3           url = /gene-fishers-cpe102-fall-08/badquiz/edit/     404                     404 is a bad link error
+			4           url = /badclass/samplequiz/edit/                     404                     404 is a bad link error
+		'''
+
+		response = self.client.get('/' + self.courseSlug + '/' + self.quizSlug + '/edit/')
+		self.failUnlessEqual(response.status_code, 200)
