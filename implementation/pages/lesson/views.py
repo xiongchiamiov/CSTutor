e333de678dccc743637f0f6240a6107149c6542a
mtytel <mtytel@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-02-08 20:18:09 +0000

    lesson creator update
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@999 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/pages/lesson/views.py
+++ implementation/pages/lesson/views.py
@@ -16,18 +16,29 @@ import re
 '''
 
 def create_lesson(request):
-	ref_url = urlparse.urlparse(request.META.get('HTTP_REFERER', ''))
-	page_slug = re.search(r"/((\w|-)+)/$", ref_url.path)
+	'''
+	@author Matthew Tytel
+
+	Creates a new lesson if name is long enough
+	'''
+
+	#ref_url = urlparse.urlparse(request.META.get('HTTP_REFERER', ''))
+	#page_slug = re.search(r"/((\w|-)+)/?$", ref_url.path)
 	
-	if page_slug is None:
-		return master_rtr(request, 'lesson/create-lesson.html')
+	#if page_slug is None:
+	#	return master_rtr(request, 'lesson/create-lesson.html')
 	
-	parent = Lesson.objects.filter(slug=page_slug.group(1))
-	if not parent:
-		return master_rtr(request, 'lesson/create-lesson.html')
-	#new = CreateLesson("Test")
+	#parent = Lesson.objects.filter(slug=page_slug.group(1))
+	#if not parent:
+	#	return master_rtr(request, 'lesson/create-lesson.html')
+	
+	#new = CreateLesson('temp')
 	#insertChildPage(new, parent[0])
-	return master_rtr(request, 'lesson/create-lesson.html')
+	return edit_new_lesson(request)
+
+def edit_new_lesson(request):
+	content = CreateLesson('New Test').content
+	return master_rtr(request, 'page/lesson/edit_lesson.html', {'content':content})
 
 def show_lesson(request, course, pid):
   # To get lessonConent now, you need to retreieve the page from the database, cast it to a lesson, and get the "text" attribute
