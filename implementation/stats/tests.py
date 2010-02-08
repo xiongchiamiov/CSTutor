4d2208902c7a3c195c2cf00e72449db9fcb53fdd
amusselm <amusselm@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-02-08 20:22:59 +0000

    Added statsTest file
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@1000 50065d94-6802-42e0-adc3-1bef15d4fe63

---
+++ implementation/stats/tests.py
@@ -0,0 +1,14 @@
+'''
+Unit tests for functions in the Stats module
+
+@author Andrew J. Musselman
+'''
+
+from django.test import TestCase
+from django.test.client import Client
+
+class StatsTests(TestCase):
+	'''
+	This class runs test for Stats functions
+	'''
+	pass
