872b96f874470aaf7d6dbe8edd8513a2746fbf07
mgius <mgius@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-02-08 17:51:09 +0000

    Added target to makefile.
    Moved stats.py to stat.py to remove name clash, fixed an import error in 
    stats.views
    
    
    
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@997 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/stats/views.py
+++ implementation/stats/views.py
@@ -6,15 +6,15 @@
 
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
-from models import Stat
 from courses.models import Course
+from stats.models import Stat
 #from stats.stats import *
-from users.models import User
+from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.template.defaultfilters import slugify 
 
 # Create your views here.
-def display_couse_stats(request, course_slug):
+def display_course_stats(request, course_slug):
 	#return render_to_response('stats/show_couse_stats.html', 
 	pass
 	#STUB... FIXME
@@ -26,6 +26,3 @@ def display_all_stats(request):
 	data['courses'] = courses 
 
 	return render_to_response('stats/show_all_stats.html',data)
-	
-
-
