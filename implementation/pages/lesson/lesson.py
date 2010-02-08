c4a57e2df78232c67aeab5ebdc1df151997020f2
jhartqui <jhartqui@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-02-08 06:36:13 +0000

    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@986 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/pages/lesson/lesson.py
+++ implementation/pages/lesson/lesson.py
@@ -4,10 +4,20 @@ lesson.py fil for lesson related operations.
 Contains operations for all lessons
 
 @author Matthew Tytel
-
+@author John Hartquist
 '''
 from django.template.defaultfilters import slugify
 from models import *
 
 def CreateLesson(name):
 	return Lesson(slug=slugify(name), name=name)
+
+def saveLesson(request, course, pid):
+	if (request.method != "POST"):
+		return -1
+	if "Save" in request.POST:
+		lesson = Lesson.objects.get(slug=pid)
+		lesson.content = request.POST["content"]
+		lesson.save()
+		return 0
+	return -1
