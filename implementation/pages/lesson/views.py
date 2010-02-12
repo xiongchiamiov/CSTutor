8cef4649c29d99111b7a659059ea8698c41dcbb7
mgius <mgius@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-02-12 22:51:24 +0000

    That should add the "edit" button to lesson pages
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@1065 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/pages/lesson/views.py
+++ implementation/pages/lesson/views.py
@@ -31,30 +31,32 @@ def create_lesson(request, course_slug, pid = ""):
 	lesson = CreateLesson('')
 	return master_rtr(request, 'page/lesson/edit_lesson.html', \
 			{'course_slug':course_slug,
-			 'page_slug': pid, \
+			 'page_slug': page_slug, \
 			 'pid':lesson.name, 'content':lesson.content, 'new':True})
 
-def show_lesson(request, course, pid):
+def show_lesson(request, course, page_slug):
   # To get lessonConent now, you need to retreieve the page from the database, cast it to a lesson, and get the "text" attribute
 	#shouldn't have to try/except because previous calls should guarentee the page exists	
-	content = Lesson.objects.get(slug=pid).content
+	content = Lesson.objects.get(slug=page_slug).content
 	return master_rtr(request, 'page/lesson/index.html', \
 			{'course':course, 'course_slug':course, \
-			 'pid':pid, 'content':content})
+			 'page_slug':page_slug, 'pid':page_slug, 'content':content})
 
-def edit_lesson(request, course, pid):
+def edit_lesson(request, course, page_slug):
 	print "EDIT"
 	if request.method == "POST":
 		if "Save" in request.POST:
-			saveLesson(request, course, pid)
+			saveLesson(request, course, page_slug)
 			return master_rtr(request, 'page/lesson/save_lesson.html', \
-					{'course':course, 'course_slug':course, 'pid':pid})
+					{'course':course, 'course_slug':course, \
+					 'page_slug':page_slug, 'pid':page_slug})
 		elif "Remove" in request.POST:
-			removeLesson(request, course, pid)
+			removeLesson(request, course, page_slug)
 			return master_rtr(request, 'page/lesson/remove_lesson.html', \
-					{'course': course, 'course_slug':course, 'pid':pid})
+					{'course': course, 'course_slug':course, \
+					 'page_slug':page_slug, 'pid':page_slug})
 	
-	content = Lesson.objects.get(slug=pid).content
+	content = Lesson.objects.get(slug=page_slug).content
 	return master_rtr(request, 'page/lesson/edit_lesson.html', \
 			{'course':course, 'course_slug':course, \
-			 'pid':pid, 'content':content})
+			 'page_slug':page_slug, 'pid':page_slug, 'content':content})
