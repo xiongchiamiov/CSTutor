b57f5bb995404ae270511c04accbbb3fb7e33068
mgius <mgius@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-03-08 20:56:30 +0000

    Adding database thing
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@1267 50065d94-6802-42e0-adc3-1bef15d4fe63

---
+++ implementation/database/__init__.py
@@ -0,0 +1,9 @@
+'''
+There is a one-to-one mapping between a model class and database tables.
+
+With the exception of the enrollment class/table, there is a one-to-one mapping
+between a package name and a class/table name.
+
+The fields of the database table have a one-to-one mapping to the attributes
+defined on each model class.  
+'''
