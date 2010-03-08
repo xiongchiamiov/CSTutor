12275f96d6b91aa857c05f215560c359b22ad660
mgius <mgius@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-03-08 00:21:58 +0000

    Woot
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@1259 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/codeshell/pythoncode.py
+++ implementation/codeshell/pythoncode.py
@@ -5,22 +5,35 @@ This could be as simple as an "eval," but it is sometimes handy to do something
 like sanitize code or capture output, etc, so we have a special execute
 function that will abstract all that garbage out
 
-SECURITY RISK: NONE OF THESE FUNCTIONS DO ANY KIND OF SANITIZING!!!!
+POTENTIAL SECURITY RISK: These functions do not currently protect against 
+importing python modules.
 '''
 
-import compiler
 
-def evalPythonCode(code):
-	'''
-	Executes compiled python code
-
-	Takes in a code object and evaluates it. Returns the result
-	'''
+from RestrictedPython import compile_restricted
+from RestrictedPython.PrintCollector import PrintCollector
 
 def evalPythonString(string):
 	'''
 	Executes strings of python code
 
-	Takes in a string and evaluates it.  returns the result
+	Takes in a string and evaluates it. 
+
+	Returns a tuple of the print output and a dictionary of the variables as
+	they existed at the end of the code execution
 	'''
-	return eval(string, {}, {})
+	string += "codeShellOutput_ = printed\n"
+
+	code = compile_restricted(string, '<string>', 'exec')
+
+	restrictedScope = {'_print_':PrintCollector, 'codeShellOutput_':''}
+	exec(code, restrictedScope)
+	
+	codeOutput = restrictedScope['codeShellOutput_']
+	del restrictedScope['codeShellOutput_']
+	del restrictedScope['_print_']
+   # These are apparently created by exec or something
+	del restrictedScope['__builtins__']
+	del restrictedScope['_print']
+
+	return (codeOutput, restrictedScope)
