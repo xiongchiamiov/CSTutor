c8945e094f8293edd1af26b0ae38431bb04c7389
pearson <pearson@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-03-09 09:28:16 +0000

    eff I'm blind
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@1291 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/codeshell/views.py
+++ implementation/codeshell/views.py
@@ -13,7 +13,7 @@ def execute(request):
 	# so... there are some things that we currently aren't dealing with,
 	# as far as handlers for things in RestrictedPython
 	# better not send a 500, as that'll just produce no result
-	except Exception as e:
+	except Exception, e:
 		#import pdb; pdb.set_trace()
 		result = "Oops!  We weren't able to process your request, so please let your instructor know so we can fix it, mmk?"
 	return HttpResponse(simplejson.dumps({'result': result}),
