709cd0cd9c65f6ce1b699d59c6611e72cd8dea5c
mgius <mgius@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-03-10 22:48:46 +0000

    More project cleanup
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@1321 50065d94-6802-42e0-adc3-1bef15d4fe63

---
+++ implementation/include/__init__.py
@@ -0,0 +1,8 @@
+'''
+The Include module is a Meta-module that contains small bits of 
+documentation for third-party products that we have incorporated
+into our project.
+
+Currently, this is the tinymce WYSIWYG HTML editor, and a small amount of 
+documentation about the django generated databases
+'''
