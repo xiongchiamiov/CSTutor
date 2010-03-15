1a332e6f63bc9ce395acf54c97c6188fedb8115b
rmezzett <rmezzett@50065d94-6802-42e0-adc3-1bef15d4fe63>
2010-03-15 00:57:58 +0000

    Added fixture for lesson tests, no more commit errors. Sorry guys
    git-svn-id: https://autotutor.csc.calpoly.edu/svn/trunk@1346 50065d94-6802-42e0-adc3-1bef15d4fe63

--- implementation/pages/lesson/tests.py
+++ implementation/pages/lesson/tests.py
@@ -16,23 +16,42 @@ class LessonTests(TestCase):
 	These functions are tested by performing changes to lessons in the fixture
 	and verifying that expected behavior results.
 	
-	TODO FIXTURE!!!
+	This fixture consists of two courses:
+		testcourseprivate has none.
+		testcoursepublic has a lesson and a quiz.
+		Two users:
+		Testuser1 has full permissions in both courses. 
+		Testuser2 has no enrollments. 
 
 	@author Russell Mezzetta
 	'''
+	
+	fixtures = ['LessonTests']
 
+	user1 = "usertestuser1"
+	user1email = "testuser1email"
+	password = "password"
+	user2 = "usertestuser2"
+	user2email = "testuser2email"
+	private_course = "testcourseprivate"
+	public_course = "testcoursepublic"
+	lessonPage = "publiclesson"
+	quizPage = "publicquiz"
+	
 	def setUp(self):
 		'''
 		Run before each test. Gets a fresh copy of the course.
+		@author Russell Mezzetta
 		'''
-		self.course = Course.objects.get(slug="large-testing-course")
+		self.course = Course.objects.get(slug=self.public_course)
 
 	def test_revertLessonChanges(self):
 		'''
 		Tests a relatively simple function which takes a lesson and copies the
 		published copy into the working copy
+		@author Russell Mezzetta
 		'''
-		l = Lesson.objects.filter(course=self.course).get(slug="testpage6")
+		l = Lesson.objects.get(course=self.course, slug=self.lessonPage)
 		content = l.content
 		#modify working copy to make sure it changes
 		l.workingCopy = "xxx"
@@ -44,8 +63,9 @@ class LessonTests(TestCase):
 		'''
 		Tests a function which takes a lesson and copies the working copy to the
 		published copy
+		@author Russell Mezzetta
 		'''
-		l = Lesson.objects.filter(course=self.course).get(slug="testpage6")
+		l = Lesson.objects.get(course=self.course, slug=self.lessonPage)
 		l.workingCopy = content = "xxx"
 		l.content="yyy"
 		r = publishLessonChanges(l)
@@ -57,19 +77,20 @@ class LessonTests(TestCase):
 		Tests a function which attempts to save a lesson as a new name. It
 		returns None if the name is already taken, otherwise it returns the
 		lesson with the updated name/slug
+		@author Russell Mezzetta
 		'''
-		l = Lesson.objects.filter(course=self.course).get(slug="testpage6")
+		l = Lesson.objects.get(course=self.course, slug=self.lessonPage)
 		#test adding a name that is too small
 		r = saveLessonName(l, "")
 		self.assertEquals(r,{'message': 'Name change failed: name must be non-empty'})
 
 		#test adding a duplicate entry
-		r = saveLessonName(l, "testpage2")
+		r = saveLessonName(l, self.quizPage)
 		self.assertEquals(r,{'message': 'Name change failed. A page with that name already exists in this course'})
 		#test adding a valid entry
-		r = saveLessonName(l, "testtestpage6")
-		self.assertEquals(r['lesson'].name,"testtestpage6")
-		self.assertEquals(r['lesson'].slug,"testtestpage6")
+		r = saveLessonName(l, "name-is-not-taken")
+		self.assertEquals(r['lesson'].name,"name-is-not-taken")
+		self.assertEquals(r['lesson'].slug,"name-is-not-taken")
 		#test adding small valid entries
 		r = saveLessonName(l,"x")
 		self.assertEquals(r['lesson'].name,"x")
@@ -81,8 +102,9 @@ class LessonTests(TestCase):
 	def test_saveLessonWorkingCopy(self):
 		'''
 		Tests a simple function that saves the working copy of a lesson
+		@author Russell Mezzetta
 		'''
-		l = Lesson.objects.filter(course=self.course).get(slug="testpage6")
+		l = Lesson.objects.get(course=self.course, slug=self.lessonPage)
 		r = saveLessonWorkingCopy(l, "abcd")
 		self.assertEquals(r.workingCopy, "abcd")
 	
